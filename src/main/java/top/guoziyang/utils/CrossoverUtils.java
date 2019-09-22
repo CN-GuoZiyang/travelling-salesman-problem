3690151911bf7fa2f0c28d874abf64d9ce42eac4
CN-GuoZiyang <guoziyang0033@gmail.com>
2019-09-23 00:02:47 +0800

    Incomplete Edge Combination

--- src/main/java/top/guoziyang/utils/CrossoverUtils.java
+++ src/main/java/top/guoziyang/utils/CrossoverUtils.java
@@ -27,8 +27,67 @@ public class CrossoverUtils {
         Individual[] individuals = new Individual[2];
         int[] randomArray = getTwoRandom(i1.getOrder().length);
         individuals[0] = singlePmx(i1, i2, randomArray);
-        individuals[1] = singlePmx(i1, i2, randomArray);
+        individuals[1] = singlePmx(i2, i1, randomArray);
         return individuals;
+    }
+
+    public static Individual[] cycleCrossover(Individual i1, Individual i2) {
+        Individual[] individuals = new Individual[2];
+        int randomNum = random.nextInt(i1.getOrder().length);
+        individuals[0] = singleCycleCrossover(i1, i2, randomNum);
+        individuals[1] = singleCycleCrossover(i2, i1, randomNum);
+        return individuals;
+    }
+
+    public static Individual[] edgeRecombination(Individual i1, Individual i2) {
+        int length = i1.getOrder().length;
+        Individual child = new Individual(length);
+        List<Integer> childList = new ArrayList<>();
+
+        EdgeTable table = new EdgeTable(length);
+        //TODO 未完成Edge Combination
+
+        int[] childOrder = childList.subList(0, 9).stream().mapToInt(Integer::valueOf).toArray();
+        child.setOrder(childOrder);
+        Individual[] individuals = new Individual[1];
+        individuals[0] = child;
+        return individuals;
+    }
+
+    private static Individual singleCycleCrossover(Individual i1, Individual i2, int randomNum) {
+        int[] i1Order = i1.getOrder();
+        List<Integer> i1List = IntStream.of(i1Order).boxed().collect(Collectors.toList());
+        int[] i2Order = i2.getOrder();
+        List<Integer> i2List = IntStream.of(i2Order).boxed().collect(Collectors.toList());
+
+        int length = i1Order.length;
+        Individual child = new Individual(length);
+        int[] childOrder = new int[length];
+        Arrays.fill(childOrder, -1);
+
+        Set<Integer> positions = new HashSet<>();
+        positions.add(randomNum);
+        int temp1 = i1List.get(randomNum);
+        int start = temp1;
+        int temp2 = i2List.get(randomNum);
+        while(temp2 != start) {
+            positions.add(i1List.indexOf(temp2));
+            temp1 = temp2;
+            temp2 = i2List.get(i1List.indexOf(temp1));
+        }
+
+        for(int temp : positions) {
+            childOrder[temp] = i1List.get(temp);
+        }
+
+        for(int i = 0; i < length; i ++) {
+            if(childOrder[i] == -1) {
+                childOrder[i] = i2Order[i];
+            }
+        }
+
+        child.setOrder(childOrder);
+        return child;
     }
 
     private static Individual singlePmx(Individual i1, Individual i2, int[] randomArray) {
@@ -126,6 +185,49 @@ public class CrossoverUtils {
             index1 = temp;
         }
         return new int[]{index1, index2};
+    }
+
+    static class EdgeTable {
+        List<EdgeRow> rows;
+        public EdgeTable(int number) {
+            rows = new ArrayList<>();
+            for(int i = 0; i < number; i ++) {
+                rows.add(new EdgeRow(i + 1));
+            }
+        }
+
+        public EdgeRow getRow(int i) {
+            for(EdgeRow row : rows) {
+                if(row.number == i) {
+                    return row;
+                }
+            }
+            return null;
+        }
+
+        public void addElement(int number, EdgeElement element) {
+            getRow(number).elements.add(element);
+        }
+    }
+
+    static class EdgeRow {
+        int number;
+        List<EdgeElement> elements;
+
+        public EdgeRow(int number) {
+            this.number = number;
+            elements = new ArrayList<>();
+        }
+    }
+
+    static class EdgeElement {
+        int number;
+        boolean common;
+
+        public EdgeElement(int number, boolean common) {
+            this.number = number;
+            this.common = common;
+        }
     }
 
 }
